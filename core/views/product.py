dc81c8c5c8ce2d04a0155f18b900070713766b91
Eduardo da Silva <eduardo.silva@gmail.com>
2024-03-28 15:23:27 -0300

    Add filter by category id on product listing

--- core/views/product.py
+++ core/views/product.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Product
@@ -7,6 +8,8 @@ from core.serializers.product import ProductSerializer, ProductDetailSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()  # pylint: disable=no-member
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["category__id"]
 
     def get_serializer_class(self):
         if self.action in ("retrieve", "list"):
